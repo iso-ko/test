e0e32745a4b782e0b53eaaefca6dff1d2a8cb9f5
iso-ko <rena.isobe.9b@stu.hosei.ac.jp>
2021-10-08 01:41:44 +0900

    [Add]omikuji.png・空文字の場合の「名無し」表示

--- omikuji/omikuji.js
+++ omikuji/omikuji.js
@@ -2,9 +2,14 @@ let name;
 let username;
 let userresult;
 
-username = prompt("お名前を教えて下さい。");
+username = prompt("お名前を教えて下さい。","");
 
 document.getElementById("name").innerHTML = username;
+
+
+if (username === "") {
+  document.getElementById("name").innerHTML = "名無し";
+}
 
 
 let rand = Math.floor( Math.random() * 5);
